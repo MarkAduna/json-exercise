9d86a80b4df97e6fbd06a3c52d6520b09f826d61
MarkA <mark.aduna@amaysim.com.au>
2018-10-26 09:49:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## How to run
+
+1. Clone repository
+2. in repository root folder run >npm install
+3. >node ./src/index.js
---
+++ src/index.js
@@ -0,0 +1,46 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhr = new XMLHttpRequest();
+
+function Get(url) {
+	xhr.open('GET', url, false);
+	xhr.send(null);
+	return xhr.responseText;
+}
+
+const url = "http://api.viki.io/v4/videos.json?app=100250a&per_page=10&page=";
+
+var hdTotal = 0;
+var nonHDTotal = 0;
+var pageNum = 0;
+
+var loopInterval = setInterval(function () {
+
+	var pageHDTrue = 0;
+	var pageHDFalse = 0;
+	pageNum++;
+
+	var obj = JSON.parse(Get(url + pageNum));
+	var response = obj.response;
+
+	for (var i in response) {
+		var hd = response[i]["flags"].hd;
+		if (hd) {
+			pageHDTrue++;
+		} else {
+			pageHDFalse++;
+	    }
+    }
+
+	hdTotal += pageHDTrue;
+	nonHDTotal += pageHDFalse;
+
+	console.log("Processing page " + pageNum);
+
+	if (obj.more === false) {
+        clearInterval(loopInterval);
+
+        console.log("\nNumber of pages processed: " + pageNum);
+        console.log("HD Titles: " + hdTotal);
+        console.log("Non HD Titles: " + nonHDTotal + "\n");
+	}
+}, 500);
